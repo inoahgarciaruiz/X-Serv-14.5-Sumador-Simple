d5c478f0cc6d5e0ff4f24c23e94cbc2c5f2d9e9a
inoahgarciaruiz <inoahgarciaruiz@gmail.com>
2018-02-26 23:41:21 +0100

    Comments needed

--- sumApp.py
+++ sumApp.py
@@ -22,6 +22,8 @@ mySocket.listen(5)
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
 
+counter = 0
+error = 0
 try:
     while True:
         print('Waiting for connections')
@@ -29,30 +31,46 @@ try:
         message = recvSocket.recv(2048)
         print('Request received:')
         print(message)
+        
+        resource = message.split()[1].decode("utf-8").split("/")[1]
+        if resource != 'favicon.ico':
+            try:
+                if counter == 0:
+                    s1 = float(resource)
+                elif counter == 1:
+                    s2 = float(resource)
 
-        try:
-            [_ , s1, s2] = message.split()[1].decode("utf-8").split("/")
-            s1_int = float(s1)
-            s2_int = float(s2)
-            error = 0
-            c = calculadora.Calculadora(float(s1), float(s2))
-        except ValueError:
-            error = 1        
-        if error == 0:
-            c = calculadora.Calculadora(float(s1), float(s2))
-            result = c.suma()
-            aux = str(s1) + " + " + str(s2) + " = " + str(result) 
-            answer = bytes('HTTP/1.1 200 OK\r\n\r\n' +
-                     '<html><body><h1>sumApp</h1><a>' + aux +
-                     '</a></body></html>\r\n', 'utf-8')
-        else:
-            answer = bytes('HTTP/1.1 400 Bad Request\r\n\r\n' +
-                     '<html><body><h1>[Usage:] http://localhost:1234/"s1/s2"</h1><a>' +
-                     '</a></body></html>\r\n', 'utf-8')
+            except ValueError:
+                error = 1
+
+        if resource != 'favicon.ico':
+            if (error == 0 and counter == 0):
+                answer = bytes('HTTP/1.1 200 OK\r\n\r\n' +
+                         '<html><body><h1>Give me another one</h1>' +
+                         '</body></html>\r\n', 'utf-8')
+                counter = 1
+
+            elif (error == 0 and counter == 1):
+                c = calculadora.Calculadora(s1, s2)
+                result = c.suma()
+                aux = str(s1) + " + " + str(s2) + " = " + str(result) 
+                answer = bytes('HTTP/1.1 200 OK\r\n\r\n' +
+                         '<html><body><h1>sumApp</h1><a>' + aux +
+                         '</a></body></html>\r\n', 'utf-8')
+                counter = 0
+
+            elif (error == 1):
+                answer = bytes('HTTP/1.1 400 Bad Request\r\n\r\n' +
+                         '<html><body><h1>[Usage:] http://localhost:1234/"s1/s2"</h1><a>' +
+                         '</a></body></html>\r\n', 'utf-8')
+                counter = 0
+                error = 0
 
         print('Answering back...')
         recvSocket.send(answer)
         recvSocket.close()
+
+
 except KeyboardInterrupt:
     print("Closing binded socket")
     mySocket.close()
